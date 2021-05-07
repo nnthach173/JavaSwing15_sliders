cfd4ca01b8b3ed80f5c80e1379ec20d70c01f812
nnthach173 <83439525+nnthach173@users.noreply.github.com>
2021-05-07 21:59:50 +0700

    1st commit

---
+++ JavaSwing15_sliders/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Jslider = GUI components that lets user enter a value
+		//			by using an adjustable sliding knob on a track
+	SliderDemo sliderDemo = new SliderDemo();
+	
+	}
+
+}
